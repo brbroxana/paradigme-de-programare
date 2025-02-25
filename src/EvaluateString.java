1a9b78ae1eb1e179e536f813fe5a3e668b75487a
brbroxana <roxana-ionela.barbaliu@student.tuiasi.ro>
2025-02-25 10:16:02 +0200

    First Commit

---
+++ src/EvaluateString.java
@@ -0,0 +1,85 @@
+import java.util.Stack;
+public class EvaluateString
+{
+    public static int evaluate(String expression)
+    {
+        char[] tokens = expression.toCharArray();
+
+        // Stiva pentru operanzi
+        Stack<Integer> values = new Stack<Integer>();
+
+        // Stiva pentru operatori
+        Stack<Character> ops = new Stack<Character>();
+
+        for (int i = 0; i < tokens.length; i++)
+        {
+            if (tokens[i] == ' ')
+                continue;
+
+            if (tokens[i] >= '0' && tokens[i] <= '9')
+            {
+                StringBuffer sbuf = new StringBuffer();
+
+                while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9')
+                    sbuf.append(tokens[i++]);
+                values.push(Integer.parseInt(sbuf.toString()));
+
+                i--;
+            }
+
+            else if (tokens[i] == '(')
+                ops.push(tokens[i]);
+
+            else if (tokens[i] == ')')
+            {
+                while (ops.peek() != '(')
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                ops.pop();
+            }
+
+            // Current token is an operator.
+            else if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*' || tokens[i] == '/')
+            {
+                while (!ops.empty() && hasPrecedence(tokens[i], ops.peek()))
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+
+                ops.push(tokens[i]);
+            }
+        }
+
+
+        while (!ops.empty())
+            values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+
+        return values.pop();
+    }
+
+    public static boolean hasPrecedence(
+            char op1, char op2)
+    {
+        if (op2 == '(' || op2 == ')')
+            return false;
+        if ((op1 == '*' || op1 == '/') &&
+                (op2 == '+' || op2 == '-'))
+            return false;
+        else
+            return true;
+    }
+
+    public static int applyOp(char op, int b, int a)
+    {
+        switch (op)
+        {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b;
+        }
+        return 0;
+    }
+
+}
